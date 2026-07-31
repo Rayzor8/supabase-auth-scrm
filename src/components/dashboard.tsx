cb8e9add1d4aea3f8cf04d5930128cc57d865cfd
Ray <rayhidayat220@gmail.com>
2026-07-31 17:33:11 +0700

    feat: format data for sales chart

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -1,30 +1,81 @@
 import { useEffect, useState } from "react";
 import supabase from "../supabase-client";
-import type { Sale } from "../types";
+import type { SaleMetrics } from "../types";
+import { Chart } from "react-charts";
 
 function Dashboard() {
-  const [salesDeals, setSalesDeals] = useState<Sale[] | null>(null);
+  const [metrics, setMetrics] = useState<SaleMetrics[] | null>(null);
 
   useEffect(() => {
-    async function getSalesDeals() {
-      const { data, error } = await supabase.from("sales_deals").select(`*`);
-
-      if (error) {
-        console.error(error);
-        return;
+    async function fetchMetrics() {
+      try {
+        const { data, error } = await supabase.from("sales_deals").select(
+          `
+          name,
+          value.sum()
+          `,
+        );
+        if (error) {
+          throw error;
+        }
+        setMetrics(data);
+      } catch (error) {
+        console.error("Error fetching metrics:", error);
       }
-
-      setSalesDeals(data);
     }
-    getSalesDeals();
+    fetchMetrics();
   }, []);
 
-  console.log(salesDeals);
+  console.log(metrics);
+
+  const chartData = [
+    {
+      label: "Sales",
+      data:
+        metrics?.map((m) => ({
+          primary: m.name,
+          secondary: m.sum,
+        })) ?? [],
+    },
+  ];
+
+  function y_max() {
+    if (metrics && metrics.length > 0) {
+      const maxSum = Math.max(...metrics.map((m) => m.sum ?? 0));
+      return maxSum + 2000;
+    }
+    return 5000;
+  }
 
   return (
     <div className="dashboard-wrapper">
       <div className="chart-container">
         <h2>Total Sales This Quarter ($)</h2>
+        <div style={{ flex: 1 }}>
+          {metrics && (
+            <Chart
+              options={{
+                data: chartData,
+                primaryAxis: {
+                  getValue(datum) {
+                    return datum.primary ?? "";
+                  },
+                  scaleType: "band",
+                },
+                secondaryAxes: [
+                  {
+                    getValue(datum) {
+                      return datum.secondary ?? 0;
+                    },
+                    scaleType: "linear",
+                    min: 0,
+                    max: y_max(),                 
+                  },
+                ],
+              }}
+            />
+          )}
+        </div>
       </div>
     </div>
   );
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,7 @@
 import type { Database } from "./supabase";
 
-export type Sale = Database["public"]["Tables"]["sales_deals"]["Row"]
+export type Sale = Database["public"]["Tables"]["sales_deals"]["Row"];
+export type SaleMetrics = {
+  name: Database["public"]["Tables"]["sales_deals"]["Row"]["name"];
+  sum: Database["public"]["Tables"]["sales_deals"]["Row"]["value"];
+};
